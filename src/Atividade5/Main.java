ca82ab3bae822a778f713096de275043a4256e11
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-04-25 16:53:56 -0300

    update

--- src/Atividade5/Main.java
+++ src/Atividade5/Main.java
@@ -7,19 +7,110 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		ProdutoWeg prod1 = new MotorEletrico();
+		MotorEletrico me = new MotorEletrico();
+		InversorFrequencia iv = new InversorFrequencia();
+		Gerador ge = new Gerador();
 		Weg w = new Weg();
+		ServicoManutencao sm = new ServicoManutencao();
+		int op = 10;
+		int opA = 10, opA2 = 10;
 
-		for (int i = 0; i < 3; i++) {
-			System.out.print("Insira o tipo do motor: ");
-			prod1.setTipo(sc.next());
+		do {
 
-			w.addProduto(prod1);
+			System.out.println("-========== MENU ==========-");
+			System.out.println(" 1 - Adicionar");
+			System.out.println(" 2 - Exibir");
+			System.out.println(" 0 - Sair");
+			System.out.print(" Insira a opção: ");
+			op = sc.nextInt();
+			System.out.println();
 
-			prod1 = new MotorEletrico();
-		}
+			if (op == 1) {
 
-		w.listaProdutos();
+				System.out.println("-===== MENU ADICIONAR =====-");
+				System.out.println(" 1 - Produtos");
+				System.out.println(" 2 - Serviços");
+				System.out.println(" 0 - Sair");
+				System.out.print(" Insira a opção: ");
+				opA = sc.nextInt();
+				System.out.println();
+
+				if (opA == 1) {
+
+					System.out.println("-=== ADICIONAR PRODUTO ===-");
+					System.out.println(" 1 - Gerador");
+					System.out.println(" 2 - Inversor de Frequencia");
+					System.out.println(" 3 - Motor Eletrico");
+					System.out.println(" 0 - Sair");
+					System.out.print(" Insira a opção: ");
+					opA2 = sc.nextInt();
+					System.out.println();
+
+					if (opA2 == 1) {
+
+						ge = new Gerador(353, "Elétrico", 10000, 220, 20000, 15000, "Energia", 50);
+						w.addGe(ge);
+
+					} else if (opA2 == 2) {
+
+						iv = new InversorFrequencia(3902, "Inversor de Frequência Industrial", 5500, 380, 100, 12000,
+								500, 3);
+						w.addIv(iv);
+
+					} else if (opA2 == 3) {
+
+						me = new MotorEletrico(9324, "Motor Elétrico de Indução Trifásico", 7500, 440, 15, 10000,
+								"Rolamento de Esferas", 92);
+						w.addMe(me);
+
+					} else {
+					}
+
+				} else if (opA == 2) {
+
+					System.out.println("-=== ADICIONAR SERVIÇO ===-");
+					System.out.println(" 1 - Manutenção");
+					System.out.println(" 0 - Sair");
+					System.out.print(" Insira a opção: ");
+					opA2 = sc.nextInt();
+					System.out.println();
+
+					if (opA2 == 1) {
+
+						sm = new ServicoManutencao("Limpeza leve", 250);
+						w.addServico(sm);
+
+					} else {
+					}
+
+				} else {
+				}
+
+			} else if (op == 2) {
+				System.out.println("-===== EXIBIR LISTAS =====-");
+				System.out.println(" 1 - Lista de Produtos");
+				System.out.println(" 2 - Lista de Manutenção");
+				System.out.println(" 0 - Sair");
+				System.out.print(" Insira a opção: ");
+				opA = sc.nextInt();
+				System.out.println();
+
+				if (opA == 1) {
+
+					System.out.println("=== Lista de Produtos ===");
+					w.listaProdutos();
+
+				} else if (opA == 2) {
+
+					System.out.println("=== Lista de Manutenção ===");
+					w.listaManutencao();
+
+				} else {
+				}
+
+			}
+
+		} while (op != 0);
 
 		sc.close();
 
--- src/Atividade5/Weg.java
+++ src/Atividade5/Weg.java
@@ -4,28 +4,84 @@ import java.util.ArrayList;
 
 public class Weg {
 
-	private ArrayList<ProdutoWeg> listaProd = new ArrayList<ProdutoWeg>();
+	private ArrayList<Gerador> ge = new ArrayList<Gerador>();
+	private ArrayList<InversorFrequencia> iv = new ArrayList<InversorFrequencia>();
+	private ArrayList<MotorEletrico> me = new ArrayList<MotorEletrico>();
 	private ArrayList<ServicoManutencao> listaManut = new ArrayList<ServicoManutencao>();
-
-	public void addProduto(ProdutoWeg prod) {
-		listaProd.add(prod);
-	}
 
 	public void addServico(ServicoManutencao servManut) {
 		listaManut.add(servManut);
 	}
 
 	public void listaProdutos() {
-		for (int i = 0; i < listaProd.size(); i++) {
-			System.out.println(listaProd.get(i).getCodigo());
-			System.out.println(listaProd.get(i).getTipo());
+		System.out.println("-======== Geradores ========-");
+		for (int i = 0; i < ge.size(); i++) {
+			System.out.println(ge.get(i).getCodigo());
+			System.out.println(ge.get(i).getTipo());
+			System.out.println(ge.get(i).getPotencia());
+			System.out.println(ge.get(i).getTensao());
+			System.out.println(ge.get(i).getCapacidade());
+			System.out.println(ge.get(i).getPreco());
+			System.out.println(ge.get(i).getCombustivel());
+			System.out.println(ge.get(i).getAutonomia());
 		}
+		System.out.println();
+		System.out.println("-= Inversores de Frequência =-");
+		for (int i = 0; i < iv.size(); i++) {
+			System.out.println(iv.get(i).getCodigo());
+			System.out.println(iv.get(i).getTipo());
+			System.out.println(iv.get(i).getPotencia());
+			System.out.println(iv.get(i).getTensao());
+			System.out.println(iv.get(i).getCapacidade());
+			System.out.println(iv.get(i).getPreco());
+			System.out.println(iv.get(i).getCapFreq());
+			System.out.println(iv.get(i).getNumeroFases());
+		}
+		System.out.println();
+		System.out.println("-===== Motores Elétricos =====-");
+		for (int i = 0; i < me.size(); i++) {
+			System.out.println(me.get(i).getCodigo());
+			System.out.println(me.get(i).getTipo());
+			System.out.println(me.get(i).getPotencia());
+			System.out.println(me.get(i).getTensao());
+			System.out.println(me.get(i).getCapacidade());
+			System.out.println(me.get(i).getPreco());
+			System.out.println(me.get(i).getTipoRolamento());
+			System.out.println(me.get(i).getEficiencia());
+		}
+		System.out.println();
 	}
 
 	public void listaManutencao() {
 		for (int i = 0; i < listaManut.size(); i++) {
 			System.out.println(listaManut.get(i).getDescricao());
 			System.out.println(listaManut.get(i).getCusto());
+		}
+		System.out.println();
+
+	}
+
+	public void addGe(Gerador prod) {
+		if (!ge.contains(prod)) {
+			ge.add(prod);
+		} else {
+			System.out.println("Já existe um gerador com o ID " + prod.getCodigo() + ".");
+		}
+	}
+
+	public void addIv(InversorFrequencia prod) {
+		if (!iv.contains(prod)) {
+			iv.add(prod);
+		} else {
+			System.out.println("Já existe um inversor de frequência com o ID " + prod.getCodigo() + ".");
+		}
+	}
+
+	public void addMe(MotorEletrico prod) {
+		if (!me.contains(prod)) {
+			me.add(prod);
+		} else {
+			System.out.println("Já existe um motor elétrico com o ID " + prod.getCodigo() + ".");
 		}
 	}
 
